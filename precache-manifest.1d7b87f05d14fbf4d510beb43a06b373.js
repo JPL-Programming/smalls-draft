dc96e70af425596738001d6d35679a7cd628da1d
jamieplamb <jamieplamb@me.com>
2021-07-27 12:56:00 +0100

    Updates

---
+++ precache-manifest.1d7b87f05d14fbf4d510beb43a06b373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4f2627d8d1599faba9b",
+    "url": "/smalls-draft/static/css/main.29101197.chunk.css"
+  },
+  {
+    "revision": "f4f2627d8d1599faba9b",
+    "url": "/smalls-draft/static/js/main.f4f2627d.chunk.js"
+  },
+  {
+    "revision": "836d5ba8b6f3df514bc5",
+    "url": "/smalls-draft/static/css/1.8695229d.chunk.css"
+  },
+  {
+    "revision": "836d5ba8b6f3df514bc5",
+    "url": "/smalls-draft/static/js/1.836d5ba8.chunk.js"
+  },
+  {
+    "revision": "e71b385b03fda6064145",
+    "url": "/smalls-draft/static/js/runtime~main.e71b385b.js"
+  },
+  {
+    "revision": "e97991ca677e256828b8adb3d9d68340",
+    "url": "/smalls-draft/index.html"
+  }
+];
